f64befe8d95715e1ab6a0acd242d4e2a71cd772c
ugurhmz <uhamzaoglu@fdnsoft.com>
2023-01-10 15:11:27 +0300

    Initial Commit

---
+++ 2.MVVM-C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.MVVM-C
+//
+//  Created by Ugur Hamzaoglu on 10.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
